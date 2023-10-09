3d8512f51b37fef51eade4e3d16b956039d0432c
Mateusz <mkaminski.contact@gmail.com>
2023-10-09 07:24:23 +0200

    added ui-slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import {configureStore} from "@reduxjs/toolkit"
+import uiReducer from './ui-slice'
 
 const store = configureStore({
-    reducer: {}
+    reducer: { ui: uiReducer}
 });
 export default store;
---
+++ src/store/ui-slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialToggleState = {
+    showCart: false,
+}
+
+const uiSlice = createSlice({
+    name: 'ui',
+    initialState: initialToggleState,
+    reducers: {
+        toggleCart(state) {
+            state.showCart = !state.showCart
+        }
+    }
+})
+
+export const uiActions = uiSlice.actions
+export default uiSlice.reducer
