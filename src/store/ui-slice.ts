2311d97ba4a39858cf7817eaf37bd1cb0e0824e5
Mateusz <mkaminski.contact@gmail.com>
2023-10-15 12:39:51 +0200

    ui-slice.ts interface

--- src/store/ui-slice.ts
+++ src/store/ui-slice.ts
@@ -1,6 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialToggleState = {
+interface UiState {
+	showCart: boolean;
+}
+
+const initialToggleState: UiState = {
 	showCart: false,
 };
 
